349879de0e5122a7b09208068f7c74daabfb6d97
Kiyo <roblkyogre@craftingcomrades.net>
2025-10-10 13:33:28 -0700

    router test

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Main from "../views/main.vue";
+import Test from "../views/test.vue";
+
+const routes = [
+  { path: "/", component: Main },
+  { path: "/test/", component: Test },
+];
+
+const router = createRouter({
+  // Provide the history implementation to use. We
+  // are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes // short for `routes: routes`
+});
+
+export default router;
---
+++ src/views/test.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>here's a second test page!</h1>
+</template>
