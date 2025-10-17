9b63dc54fd6c58fb510072ee5b9421bd6d46ae10
Kiyo <roblkyogre@craftingcomrades.net>
2025-10-17 13:16:28 -0700

    import css after app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import "./main.css";
 import App from "./App.vue";
+import "./main.css";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
